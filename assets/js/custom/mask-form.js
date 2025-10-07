2c70fac0c3d4277e31fe74301910624d5ddee013
MichasBaiano <programamichel@gmail.com>
2025-10-07 12:37:18 -0300

    Portfolio, Contato e Footer estilizados
    Estilização utilizando JQuery e seu plugin Mask, além do Owl Carousel para criação do carrossel, Google Fonts para palavras e Font Awesome para os icons

---
+++ assets/js/custom/mask-form.js
@@ -0,0 +1,8 @@
+$('#whatsapp').mask('(00) 00000-0000').on('blur', function(){
+  var phone = $(this).val().replace(/\D/g, '');
+  if(phone.length === 10) {
+    $(this).mask('(00) 0000-0000');
+  } else {
+    $(this).mask('(00) 00000-0000');
+  }
+});
